89929ad24922e0c9d5ae96f035847ebac8561843
Yash Gupta <yashg160@gmail.com>
2019-12-01 18:52:08 +0530

    Users GET with query working

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -6,12 +6,36 @@ usersRouter.use(bodyParser.json());
 
 usersRouter.route('/')
     .get((req, res, next) => {
-        console.log(req.query.email);
+
+        console.log(req.query);
+
         const query = `SELECT * FROM USERS WHERE email = '${req.query.email}';`;
         console.log(query);
+
         db.query(query, (error, results, fields) => {
-            console.log(results);
-            res.status(200).send({ done: 'success'});
+            if (error) {
+                //Some error occurred while running the query
+                console.error(error);
+                res.status(400).send({ error: true, errorMessage: 'ERR_SERVER', userExists: false, email: req.query.email, password: req.query.password });
+            }
+            else {
+                //No error occurred. Proceed to check if the user's account is valid
+                console.log(results);
+                if (results.length > 1) {
+                    res.status(200).send({ error: true, errorMessage: 'ERR_DUP_ACC', userExists: true, email: req.query.email, password: req.query.password });
+                }
+                else if (results.length < 1) {
+                    res.status(200).send({ error: true, errorMessage: 'ERR_USER_EXISTS', userExists: false, email: req.query.email, password: req.query.password });
+                }
+                else {
+                    //User exists. Verify the password to confirm the identity.
+                    if (req.query.password == results[0].password)
+                        res.status(200).send({ error: false, errorMessage: 'ERR_NONE', userExists: true, email: req.query.email, password: req.query.password });
+                    else
+                        res.status(403).send({ error: true, errorMessage: 'ERR_PASSWORD', userExists: true, email: req.query.email, password: req.query.password });
+                }
+            }
+            
         });
     })
     .post((req, res, next) => {
