9c7f2778f0123597786c2c6166f45dfed6e7ef70
yashg160 <yashg160@gmail.com>
2020-05-07 17:18:09 +0530

    Changed database url

--- models/Article.js
+++ models/Article.js
@@ -1,12 +1,14 @@
 const Sequelize = require('sequelize');
 const Model = Sequelize.Model;
 
-const sequelize = new Sequelize({
+/* const sequelize = new Sequelize({
     dialect: 'mysql',
     database: 'reader',
     username: 'root',
     password: 'root_password'
-});
+}); */
+
+const sequelize = new Sequelize("mysql://b6b61469288269:a42efa6b@us-cdbr-east-06.cleardb.net/heroku_e2a64bd5a684d4a?reconnect=true");
 
 class Article extends Model { }
 
